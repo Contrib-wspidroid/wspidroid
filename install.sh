e50985721dcf07ea2ea8020bdc1c70558aba7514
domi77185 <tel.domi@gmail.com>
2015-02-24 20:29:38 +0100

    Update

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Installation de l'utilitaire psutil.
+sudo apt-get install gcc python-pip build-essential python-dev
+sudo pip install psutil
---
+++ scripts/psutil.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Import libraries Python
+import psutil
+# Execution de la commande passee en argument.
+valretour = str(sys.argv[1])
+# On retour la valeur.
+print (valretour)
