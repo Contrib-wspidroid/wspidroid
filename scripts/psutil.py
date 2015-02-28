d12bef4259757fbea168550cbb0fe8cbbcbc2ad3
domi77185 <tel.domi@gmail.com>
2015-02-28 13:44:35 +0100

    Update

--- scripts/psutil.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-# Import libraries Python
-import psutil
-# Execution de la commande passee en argument.
-valretour = str(sys.argv[1])
-# On retour la valeur.
-print (valretour)
